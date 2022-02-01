0e708eb08d6fff5e9fe84c774a2e4e4d099a9935
DoshSempai <crimson15dream@gmail.com>
2022-02-01 18:15:48 +0300

    exersice

--- src/view/components/content/Content.tsx
+++ src/view/components/content/Content.tsx
@@ -1,18 +1,10 @@
 import React, { ReactNode } from 'react';
-import { Card } from '../card/Сard';
 import './Content.css';
-
-const cardCount = [1, 1, 1, 1, 1, 1, 1, 1];
 
 interface ContentProps {
 	children?: ReactNode;
 }
 
 export const Content = ({ children }: ContentProps): JSX.Element => (
-	<div className="content">
-		{children}
-		{/* {cardCount.map((item) => (
-			<Card key={item} title={} />
-		))} */}
-	</div>
+	<div className="content">{children}</div>
 );
--- src/view/pages/exercise/Exercise.tsx
+++ src/view/pages/exercise/Exercise.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Content } from '../../components/content/Content';
+import { ExerciseButton } from '../../components/exersices/button-exersice/Button-exersice';
+import { ExerciseWrap } from '../../components/exersices/exercise-wrap/Exercise-wrap';
 import { Header } from '../../components/header/Header';
 import { Navigation } from '../../components/navigation/Navigation';
 
@@ -8,7 +10,11 @@ const Exercise = (): JSX.Element => (
 		<Navigation />
 		<div className="app_right">
 			<Header />
-			<Content />
+			<Content>
+				<ExerciseWrap>
+					<ExerciseButton title={'Выберите правильный ответ'} question={'question'} />
+				</ExerciseWrap>
+			</Content>
 		</div>
 	</>
 );
