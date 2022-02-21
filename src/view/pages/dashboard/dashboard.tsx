fc732d073a5b617550af0a27a5a171998af7cb56
Vladzavr <vlad8zavr@gmail.com>
2022-02-21 16:47:36 +0300

    [test_wizard] Temp LocalStorage as Backend

--- src/view/pages/dashboard/dashboard.tsx
+++ src/view/pages/dashboard/dashboard.tsx
@@ -1,20 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Card } from '../../components/card/Сard';
 import { dashboardTestLocalData, ITestData } from '../../../data/dashboard-data';
 import { CommonLayout } from '../common/CommonLayout';
 import { TestWizard } from '../../components/testwizard/testwizard';
+import { ApiLocalStorage } from '../../../service/api.localstorage';
 
 const Dashboard = (): JSX.Element => {
-	const [showTestWIzardModal, setShowTestWizardModal] = useState<boolean>(true);
+	const [showTestWIzardModal, setShowTestWizardModal] = useState<boolean>(false);
+	const [testList, setTestList] = useState<ITestData[]>(dashboardTestLocalData);
+	const [api] = useState(new ApiLocalStorage());
+
+	const updateList = (): void => {
+		const storageData = api.getData();
+		console.log('~~ storageData', storageData);
+		setTestList([...testList, ...storageData]);
+	};
+
+	useEffect(() => {
+		console.log('>> dashboard mount');
+		updateList();
+	}, []);
+
 	const onCreateTest = (data: ITestData): void => {
 		console.log('onCreateTest', data);
-		console.log('onCreateTest', JSON.stringify(data));
+		api.writeData(data);
+		updateList();
+		setShowTestWizardModal(false);
 	};
+
 	return (
 		<>
 			<CommonLayout onCreateTestClick={(): void => setShowTestWizardModal(true)}>
-				{dashboardTestLocalData.map((testData) => (
+				{testList.map((testData) => (
 					<Link
 						key={testData.title}
 						to="/exercise"
