60650122a2e6224bfa7f8873e0ef893e70ea2e72
Vladzavr <vlad8zavr@gmail.com>
2022-02-23 14:35:11 +0300

    [search_test] Action With no Actions

--- src/view/components/card/Сard.tsx
+++ src/view/components/card/Сard.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
+import { DeleteIcon, SettingsIcon, StatisticsIcon } from '../../../assets/svg';
 import './Card.css';
 
 interface CardProps {
 	title: String;
 }
 
-export const Card = ({ title }: CardProps): JSX.Element => (
-	<div className="card">
-		<div className="card__content">
-			<div className="card__title">{title}</div>
+export const Card = ({ title }: CardProps): JSX.Element => {
+	const handleActionClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+		e.preventDefault();
+		e.stopPropagation();
+		console.log(`action !!`);
+	};
+
+	return (
+		<div className="card">
+			<div className="card__content">
+				<div className="card__title">{title}</div>
+				<div className="card__actions">
+					<div className="card__action" onClick={handleActionClick}>
+						<StatisticsIcon />
+					</div>
+					<div className="card__action" onClick={handleActionClick}>
+						<SettingsIcon />
+					</div>
+					<div className="card__action" onClick={handleActionClick}>
+						<DeleteIcon />
+					</div>
+				</div>
+			</div>
+			<div className="card__navigation"></div>
 		</div>
-		<div className="card__navigation"></div>
-	</div>
-);
+	);
+};
