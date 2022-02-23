a7efa64be834a77025cbff018117b036846ae137
Vladzavr <vlad8zavr@gmail.com>
2022-02-23 23:05:45 +0300

    [search_test] Export csv example

--- src/view/pages/statistics/statistics.tsx
+++ src/view/pages/statistics/statistics.tsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import cn from 'classnames';
+import { CSVLink } from 'react-csv';
 import { Table } from '../../components/table/table';
 import { CommonLayout } from '../common/CommonLayout';
 import { Histogram } from '../../components/histogram/histogram';
+import { DownloadIcon } from '../../../assets/svg';
 
 enum Tabs {
 	Table,
 	Hist,
 }
+
+const csvData = [
+	['firstname', 'lastname', 'email'],
+	['Ahmed', 'Tomi', 'ah@smthing.co.com'],
+	['Raed', 'Labes', 'rl@smthing.co.com'],
+	['Yezzi', 'Min l3b', 'ymin@cocococo.com'],
+];
 
 export const Statistics = (): JSX.Element => {
 	const [activeTab, setActiveTab] = useState<Tabs>(Tabs.Table);
@@ -23,6 +32,11 @@ export const Statistics = (): JSX.Element => {
 				<div className="header-with-controls">
 					<div className="st-h2">Результаты</div>
 					<div className="st-tabs">
+						<div className="card__action" style={{ marginRight: '12px' }}>
+							<CSVLink data={csvData}>
+								<DownloadIcon />
+							</CSVLink>
+						</div>
 						<div
 							className={cn('st-tab', {
 								'st-tab--active': activeTab === Tabs.Table,
